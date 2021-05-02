764eb938be8eca1b02dbdb028f47d6373ed70c07
이창우 <cwadven@naver.com>
2021-05-02 22:54:13 +0900

    react-slick 추가

--- src/views/component/CustomCard.js
+++ src/views/component/CustomCard.js
@@ -1,6 +1,9 @@
 import './../../assets/scss/component/CustomCard.scss';
 import {useState} from 'react';
 import {Card} from 'react-bootstrap';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
 const CustomCard = ({array}) => {
     const [card, setCard] = useState(array);
@@ -22,10 +25,45 @@ const CustomCard = ({array}) => {
         });
     };
 
+    const settings = {
+        dots: true,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        initialSlide: 0,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+              initialSlide: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+    };
+
     return(
-        <div style={{ display: 'flex'}}>
+        <Slider slidesToShow={5} slidesToScroll={5} initialSlide={0}>
             {mapToComponent(card)}
-        </div>
+        </Slider>
     )
 }
 
